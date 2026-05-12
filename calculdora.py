e11b95ebc2db40cfc7cef4b5b6471b91edbcd327
Yasmin <y.santana@edu.senai.br>
2026-05-12 15:15:54 -0300

    Arquivo alterado = cabeçalho removido

--- calculdora.py
+++ calculdora.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Meu Primeiro Programa em Python
-Autor: [Seu Nome]
-Data: [Data de Hoje]
+Autor: Yasmin Tavares Santana
+Data: 12/05/26
 Descrição: Programa básico para aprender GitHub
 """
 
@@ -24,12 +24,12 @@ def boas_vindas():
     print("   Bem-vindo ao GitHub!\n")
     
     # 2. Informações pessoais
-    nome = "[Seu Nome]"
-    turma = "[Turma]"
+    nome = Yasmin Tavares Santana
+    turma = 2° A
     
     print("2️⃣ Sobre mim:")
-    print(f"   Nome: {nome}")
-    print(f"   Turma: {turma}")
+    print(f"   Nome: Yasmin")
+    print(f"   Turma: 2° A")
     print()
 
     # 3. Data e hora atual
